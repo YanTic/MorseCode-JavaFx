f0ad435e4d29950c5e92f2a7244152e9c9c86c87
Julian Acosta <acostajuliant@gmail.com>
2021-03-02 10:18:51 -0500

    Change scenes y new buttons

--- src/main/java/MorseController.java
+++ src/main/java/MorseController.java
@@ -1,29 +1,40 @@
 package main.java;
 
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class MorseController {
-
-    @FXML
-    private Button dotButton;
-
-    @FXML
-    private Button dashButton;
-
-    @FXML
-    private TextField morseText;
-
-    @FXML
-    void dashEvent(ActionEvent event) {
-        System.out.println("Dash");
-    }
+    @FXML private Button returnButton;
+    @FXML private Button dashButton;
+    @FXML private Button dotButton;
+    @FXML private TextField morseText;
 
     @FXML
     void dotEvent(ActionEvent event) {
         System.out.println("Dot");
+        morseText.setText(morseText.getText() + ".");
+    }
+
+    @FXML
+    void dashEvent(ActionEvent event) {
+        System.out.println("Dash");
+        morseText.setText(morseText.getText() + "-");
+    }
+
+    @FXML
+    void returnBttEvent(ActionEvent event) throws IOException {
+        // This change the scene, return to the main scene
+        Parent root = FXMLLoader.load(getClass().getResource("/resources/view/MainView.fxml"));
+        Stage mainView = (Stage) returnButton.getScene().getWindow();
+        mainView.setScene(new Scene(root, 800, 600));
     }
 
 }
